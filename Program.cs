0219388de652facb938a92708cb88fa93c0939c1
WillianMacedoW <macedow121@gmail.com>
2024-03-14 21:08:42 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("oi");
+﻿//Crie uma variável chamada idade e atribua a ela o valor 35 e exiba o seu valor.
+byte idade = 35;
+Console.WriteLine(idade);
+
+//Crie uma variável chamada nome e atribua o valor "Maria" e exiba o seu valor.
+string nome = "maria";
+Console.WriteLine(nome);
+
+//Crie uma variável chamada altura e atribua a ela o valor 3.45.
+
+float altura = 3.45f;
+Console.WriteLine(altura);
+
+//Crie uma constante chamada ano e atribua a ela o valor 12 e exiba o seu valor
+
+const byte ano = 12;
+Console.WriteLine(ano);
+
+//Declare uma variável nota do tipo double como sendo do tipo Nullable type e atribua o valor 7.80 e exiba o seu valor
+double? nota = null;
+nota = 7.8;
+Console.WriteLine(nota);
+
+//Quais as diferenças entre os tipos por valor e os tipos por referência ?
+//Variáveis de tipos de referência armazenam referências em seus dados (objetos) 
+//enquanto que variáveis de tipos de valor contém diretamente seus dados.
+
+//O que é um nullable type e qual a sua utilidade ?
+//é um recurso do C# que nos permite atribuir o valor null a um tipo de dado que, por padrão, não aceita valores nulos: 
+//os tipos primitivos.
+
+//O que é Camel Case ? Dê um exemplo de sua aplicação.
+//Por exemplo, se quisermos nomear uma variável que represente a idade de uma pessoa, 
+//podemos chamá-la de "idadeDaPessoa" em camel case. Em C#, a convenção de nomenclatura camel case é usada para nomear variáveis, 
+//parâmetros de método e propriedades.
+
+//O que é Pascal Case ? Dê um exemplo de sua aplicação
+// Também conhecido como “upper camel case” ou “capital case”, pascal case combina palavras colocando todas com a primeira letra maiúscula:
+// Ex: CoisasParaFazer
+
+//Declare duas variáveis x e y como sendo do tipo int e atribua a ela os valores 77 e 66 e a
+//seguir imprima o valor da soma de x com y. (Use o operador aritmético + para realizar a
+//operação de soma)
+
+int x = 77; 
+int y = 66;
+
+int nota1 = y+x;
+Console.WriteLine(nota1);
+
+
+
+
+
+
 
